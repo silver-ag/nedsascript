5279e5a9a1e0222c876bf9e1c75872ad9ed2dd2e
Asa <asaglenister@protonmail.com>
2024-02-09 21:24:37 +0000

    first commit: language seems to work, basically untested, no input

---
+++ nedsascript.py
@@ -0,0 +1,181 @@
+from lark import Lark, Transformer, Visitor, Tree, Token, Discard
+from stackautomaton import NonErasingStackAutomaton, Transition
+import itertools
+
+with open('nedsascript_grammar.lark', 'r') as grammar:
+    nedsascript_parser = Lark(grammar, start = 'program')
+
+def parse(code):
+    initial_tree = nedsascript_parser.parse(code)
+    processed_tree = EndReplacer().transform(initial_tree)
+    return processed_tree
+
+def construct_nedsa(code):
+    new_tree, variables, states, alphabet = Preprocessor().transform(parse(code))
+    variable_possibilities = list(itertools.product(*[list(range(v[1])) for v in variables.values()]))
+    variable_names = list(variables.keys()) # tells us what order variable_possibilities is in
+    variable_initialisations = [v[0] for v in variables.values()]
+    return parse_program(new_tree, alphabet, variable_possibilities, variable_names, variable_initialisations)
+
+def state_endings(variable_possibilities):
+    # make a list of all possible states in the form <statename>_<val1>_<val2> ... for values given as possible
+    r = []
+    for possibility in variable_possibilities:
+        r.append('')
+        for variable in possibility:
+            r[-1] += '_' + str(variable)
+    return r
+
+class EndReplacer(Transformer):
+    # replace any initial codeblock with a labelledcodeblock with the label +FIRSTLABEL (note the + makes it not a valid user-declared label)
+    # and replace any final label with a labelledcodeblock with an empty codeblock
+    def program(self, items):
+        if isinstance(items[0], Tree) and items[0].data == 'codeblock':
+            items[0] = Tree(Token('RULE', 'labelledcodeblock'), [Tree(Token('RULE', 'label'), [Token('NAME', '+FIRSTLABEL')])] + items[0].children)
+        if isinstance(items[-1], Tree) and items[-1].data == 'label':
+            items[-1] = Tree(Token('RULE', 'labelledcodeblock'), [items[-1]])
+        return Tree(Token('RULE', 'program'), items)
+
+class Preprocessor(Transformer):
+    # remove all var declarations and record them, state names and alphabet seperately
+    def __init__(self):
+        super().__init__()
+        self.vars = {}
+        self.states = []
+        self.alphabet = set(['BLANK'])
+    def transform(self, tree):
+        new_tree = super().transform(tree)
+        return (new_tree, self.vars, self.states, self.alphabet)
+    def labelledcodeblock(self, items):
+        label = items[0].children[0].value
+        if label in self.states:
+            raise(ParseException(f"parse error: label '{label}' declared twice"))
+        else:
+            self.states.append(label)
+        return Tree(Token('RULE', 'labelledcodeblock'), items)
+    def vardeclaration(self, items):
+        name = items[0].value
+        value = items[1]
+        maximum = items[2]
+        if name in self.vars:
+            raise(ParseException(f"parse error: variable '{name}' declared twice"))
+        elif value > maximum:
+            raise(ParseException(f"parse error: variable '{name}' initialised to a larger value than its declared maximum ({value} > {maximum})"))
+        else:
+            self.vars[name] = (value, maximum)
+            return Discard
+    def push(self, items):
+        self.alphabet.add(items[0].value)
+        return Tree(Token('RULE', 'push'), items)
+    def ifread(self, items):
+        self.alphabet.add(items[2].value)
+        return Tree(Token('RULE', 'ifread'), items)
+    def number(self, items):
+        return int(items[0].value)
+
+def parse_program(program, alphabet, variable_possibilities, variable_names, variable_initialisations):
+    transitions = []
+    initial_ending = ''
+    for init in variable_initialisations:
+        initial_ending += '_' + str(init)
+    transitions.append(Transition('+START+', 'BLANK', program.children[0].children[0].children[0].value + initial_ending, {}))
+    for i in range(len(program.children)):
+        new_transitions, final_n = parse_labelled_block(program.children[i], alphabet, variable_possibilities, variable_names)
+        transitions += new_transitions
+        if i + 1 < len(program.children):
+            for ending in state_endings(variable_possibilities):
+                for symbol in alphabet:
+                    transitions.append(Transition(program.children[i].children[0].children[0].value + ending + 'block' + str(final_n), symbol,
+                                                  program.children[i+1].children[0].children[0].value + ending, {}))
+    return NonErasingStackAutomaton(transitions)
+        
+
+def parse_labelled_block(block, alphabet, variable_possibilities, variable_names):
+    transitions = []
+    label = block.children[0].children[0].value
+    for ending in state_endings(variable_possibilities):
+        for symbol in alphabet:
+            transitions.append(Transition(label + ending, symbol, label + ending + 'block0', {}))
+    new_transitions, new_n = parse_codeblock(block.children[1:], label, alphabet, variable_possibilities, variable_names, 0)
+    transitions += new_transitions
+    return transitions, new_n
+
+def parse_codeblock(block, label, alphabet, variable_possibilities, var_names, n):
+    transitions = []
+    for statement in block:
+        if statement.data == 'pass':
+            pass
+        elif statement.data == 'push':
+            for ending in state_endings(variable_possibilities):
+                for symbol in alphabet:
+                    transitions.append(Transition(label + ending + 'block' + str(n), symbol, label + ending + 'block' + str(n+1), {'push': statement.children[0].value}))
+            n += 1
+        elif statement.data == 'move':
+            for ending in state_endings(variable_possibilities):
+                for symbol in alphabet:
+                    transitions.append(Transition(label + ending + 'block' + str(n), symbol, label + ending + 'block' + str(n+1),
+                                               {'move': {'DOWN':'left', 'UP':'right'}[statement.children[0].value]}))
+            n += 1
+        elif statement.data == 'halt':
+            for ending in state_endings(variable_possibilities):
+                for symbol in alphabet:
+                    transitions.append(Transition(label + ending + 'block' + str(n), symbol, label + ending + 'block' + str(n), {'halt': True}))
+        elif statement.data == 'goto':
+            for ending in state_endings(variable_possibilities):
+                for symbol in alphabet:
+                    transitions.append(Transition(label + ending + 'block' + str(n), symbol, statement.children[0].value + ending, {}))
+        elif statement.data == 'ifread':
+            read_symbol = statement.children[1].value
+            for ending in state_endings(variable_possibilities):
+                transitions.append(Transition(label + ending + 'block' + str(n), read_symbol, label + ending + 'block' + str(n+1), {}))
+            new_transitions, new_n = parse_codeblock(statement.children[2].children, label, alphabet, var_possibilities, var_names, n + 1)
+            transitions += new_transitions
+            for ending in state_endings(variable_possibilities):
+                for symbol in alphabet:
+                    if symbol != read_symbol:
+                        transitions.append(Transition(label + ending + 'block' + str(n), symbol, label + ending + 'block' + str(new_n), {}))
+            n = new_n
+        elif statement.data == 'ifcomparison':
+            constrained_possibilities = constrain_possibilities(variable_possibilities, var_names, statement.children[0], statement.children[1], statement.children[2])
+            for ending in state_endings(constrained_possibilities):
+                for symbol in alphabet:
+                    transitions.append(Transition(label + ending + 'block' + str(n), symbol, label + ending + 'block' + str(n+1), {}))
+            new_transitions, new_n = parse_codeblock(statement.children[3].children, label, alphabet, constrained_possibilities, var_names, n + 1)
+            transitions += new_transitions
+            for ending in state_endings(set(variable_possibilities) - set(constrained_possibilities)):
+                for symbol in alphabet:
+                    transitions.append(Transition(label + ending + 'block' + str(n), symbol, label + ending + 'block' + str(new_n), {}))
+    return transitions, n
+
+
+def constrain_possibilities(var_possibilities, var_names, a, comparator, b):
+    evaluator = ExpressionEvaluator(var_names)
+    fa = evaluator.transform(a)
+    fb = evaluator.transform(b)
+    comp = {'=': lambda a,b: a == b, '>': lambda a,b: a > b, '<': lambda a,b: a < b, '>=': lambda a,b: a >= b, '<=': lambda a,b: a <= b, '!=': lambda a,b: a != b}[comparator]
+    r = []
+    for possibility in var_possibilities:
+        if comp(fa(possibility), fb(possibility)):
+            r.append(possibility)
+    return r
+
+
+class ExpressionEvaluator(Transformer):
+    # constructs a function that takes a list of variable values and produces an expression value
+    def __init__(self, variable_names):
+        super().__init__()
+        self.variable_names = variable_names
+    def expression(self, items):
+        if isinstance(items[0], Token) and items[0].type == 'NAME': # variable name
+            return lambda variables: variables[self.variable_names.index(items[0])]
+        elif len(items) == 3: # operation
+            operation = {'+': lambda a,b: a+b, '-': lambda a,b: a-b, '*': lambda a,b: a*b, '/': lambda a,b: a//b}[items[1].value]
+            return lambda variables: operation(items[0](variables), items[2](variables))
+        else: # in all other cases it's already been processed into a list of numbers
+            return lambda variables: items[0]
+    
+
+class ParseException(Exception):
+    pass
+
+nedsa = construct_nedsa("VAR testvara = 2 / 3\nVAR testvarb = 2 / 10\ntestvara = testvarb + 1\n:loop\nPUSH hi\nIF 1 + 1 = testvarb { GOTO loop }")
---
+++ stackautomaton.py
@@ -0,0 +1,133 @@
+
+class Transition:
+    def __init__(self, state_from, value_from, state_to, side_effects):
+        self.state_from = state_from
+        self.value_from = value_from
+        self.state_to = state_to
+        self.push = side_effects['push'] if 'push' in side_effects else None
+        self.move = {'left': -1, 'right': 1}[side_effects['move']] if 'move' in side_effects else 0
+
+class TableTransition:
+    # elements of the transition table can't push or move, but they do need to be able to halt before reaching the end
+    def __init__(self, state_from, state_to, halt = False):
+        self.state_from = state_from
+        self.state_to = state_to
+        self.halt = halt
+    def __eq__(self, other):
+        # required so that transition tables can be compared for equality
+        if isinstance(other, TableTransition):
+            return self.state_from == other.state_from and self.state_to == other.state_to and self.halt == other.halt
+        else:
+            return False
+        
+
+class NonErasingStackAutomaton:
+    # inputless version, because that's all i need
+    def __init__(self, transitions):
+        self.transitions = {(transition.state_from, transition.value_from) : transition for transition in transitions}
+        self.states = set()
+        self.alphabet = set(['BLANK'])
+        for transition in transitions:
+            self.states.add(transition.state_from)
+            self.states.add(transition.state_to)
+            self.alphabet.add(transition.value_from)
+    def run(self, state):
+        pointer = 0
+        stack = []
+        while True:
+            print(f"{stack}:{pointer} in {state}")
+            if (state, stack[pointer] if pointer < len(stack) else 'BLANK') in self.transitions:
+                t = self.transitions[(state, stack[pointer] if pointer < len(stack) else 'BLANK')]
+            else:
+                # if there's no transition for this situation, halt and return the current state
+                return state
+            state = t.state_to
+            if t.push is not None:
+                if pointer == len(stack):
+                    stack += [t.push]
+                    pointer += 1
+                else:
+                    # pushing while not at the top of the stack is rejecting
+                    return 'reject: push while not at top'
+            pointer += t.move
+            if pointer < 0 or pointer > len(stack):
+                # moving the pointer off the stack is rejecting
+                return 'reject: moved pointer off stack'
+    def make_transition_table(self, prev_transition_table, new_value):
+        return {state: self.get_eventual_transition(state, prev_transition_table, new_value) for state in self.states}
+    def get_eventual_transition(self, state, prev_transition_table, new_value):
+        original_state = state
+        for i in range(len(self.states) + 1):
+            if (state, new_value) in self.transitions:
+                t = self.transitions[(state, new_value)]
+            else:
+                # if there's no transition for this situation, halts and returns the reached state
+                return TableTransition(original_state, state, halt = True)
+            state = t.state_to
+            if t.push is not None:
+                # pushing while not at the top of the stack is rejecting
+                return TableTransition(original_state, 'reject: push while not at top', halt = True)
+            if t.move == 1:
+                return TableTransition(original_state, state)
+            if t.move == -1:
+                if prev_transition_table[state].halt:
+                    return TableTransition(original_state, prev_transition_table[state].state_to, halt = True)
+                else:
+                    state = prev_transition_table[state].state_to
+        return TableTransition(original_state, 'reject: infinite loop', halt = True)
+    @property
+    def first_transition_table(self):
+        return {state: TableTransition(state, 'reject: moved pointer off stack', halt = True) for state in self.states}
+    def run_with_transition_tables(self, state):
+        # guaranteed to halt!! (in exponential time, tbf)
+        # see: Nonerasing Stack Automata, J. Hopcroft, J. Ullman, Journal of Computer and Systems Sciences, 1 (1967), pp. 166-186
+        # https://www.sciencedirect.com/science/article/pii/S0022000067800138
+        # especially section 5 theorem 1
+        # we detect infinite runs that don't loop by detecting equivalent transition tables
+        # even if the stack is different, if we're in the same state with the same current transition table that's a loop,
+        # because stacks with the same transition table are indistinguishable by the automaton.
+        # this will always happen eventually because there are finitely many different transition tables for n states
+
+        # estimate worst case runtime:
+        print(f"may have to run through {pow(len(self.states), 2) * 2} tables in the worst case")
+
+        current_transition_table = self.first_transition_table
+        all_transition_tables = [(current_transition_table, [])] # have to use an assoc-list because dicts aren't valid dict keys
+        while True:
+            #print(all_transition_tables)
+            if (state, 'BLANK') in self.transitions:
+                t = self.transitions[(state, 'BLANK')]
+            else:
+                # if there's no transition for this situation, halt and return the current state
+                return state
+            state = t.state_to
+            if t.push is not None:
+                current_transition_table = self.make_transition_table(current_transition_table, t.push)
+                table_index = assoc_index(current_transition_table, all_transition_tables)
+                if table_index == -1:
+                    all_transition_tables.append((current_transition_table, []))
+            if t.move == 1:
+                return 'reject: moved pointer off stack'
+            if t.move == -1:
+                if current_transition_table[state].halt:
+                   return current_transition_table[state].state_to
+                else:
+                    state = current_transition_table[state].state_to
+                
+            table_index = assoc_index(current_transition_table, all_transition_tables)
+            if state in all_transition_tables[table_index][1]:
+                # non-halting program detected
+                return 'reject: never halts'
+            else:
+                all_transition_tables[table_index] = (all_transition_tables[table_index][0], all_transition_tables[table_index][1] + [state])
+            
+
+def assoc_index(v, al):
+    for i in range(len(al)):
+        if al[i][0] == v:
+            return i
+    return -1
+
+# tests
+#test = NonErasingStackAutomaton([Transition('a', 'BLANK', 'b', {'push': 'X'}), Transition('b', 'X', 'a', {'move': 'left'})])
+#testxyzforever = NonErasingStackAutomaton([Transition('a', 'BLANK', 'b', {'push': 'X'}), Transition('b', 'BLANK', 'c', {'push': 'Y'}), Transition('c', 'BLANK', 'a', {'push': 'Z'})])
